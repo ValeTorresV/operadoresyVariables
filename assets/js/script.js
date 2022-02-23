87ed78ecc3b8b4584871d798f916ee62060079ae
Valeska Torres V <valeska.t.valenzuela@gmail.com>
2022-02-23 18:04:35 -0300

    operaciones Bàsicas

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+/*operaciones con nùmeros*/
+
+/*Suma*/
+
+var n1 = prompt ("Ingrese un primer numero entero");
+var n1 = parseInt(n1);
+var n2 = prompt ("ingrese un segundo numero entero diferente del primero");
+var n2 = parseInt(n2);
+alert(n1+n2)
+
+
+/*Resta*/
+var n1 = prompt ("Ingrese un primer numero entero");
+var n1 = parseInt(n1);
+var n2 = prompt ("ingrese un segundo numero entero diferente del primero");
+var n2 = parseInt(n2);
+alert(n1-n2)
+
+
+/*División*/
+
+var n1 = prompt ("Ingrese un primer numero entero");
+var n1 = parseInt(n1);
+var n2 = prompt ("ingrese un segundo numero entero diferente del primero");
+var n2 = parseInt(n2);
+alert(n1/n2)
+git 
+/*Multiplicación*/
+
+var n1 = prompt ("Ingrese un primer numero entero");
+var n1 = parseInt(n1);
+var n2 = prompt ("ingrese un segundo numero entero diferente del primero");
+var n2 = parseInt(n2);
+alert(n1*n2)
+
+/*Módulo*/
+
+var n1 = prompt ("Ingrese un primer numero entero");
+var n1 = parseInt(n1);
+var n2 = prompt ("ingrese un segundo numero entero diferente del primero");
+var n2 = parseInt(n2);
+alert(n1%n2)
