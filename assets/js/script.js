d4655ff77167d07a7893abe65850f5d0882d9f33
Valeska Torres V <valeska.t.valenzuela@gmail.com>
2022-02-24 18:05:27 -0300

    operaciones Basicas y temperatura

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,19 @@
 
 /*Suma*/
 
-var n1 = prompt ("Ingrese un primer numero entero");
-var n1 = parseInt(n1);
-var n2 = prompt ("ingrese un segundo numero entero diferente del primero");
-var n2 = parseInt(n2);
-alert(n1+n2)
+let s1 = prompt ("Ingrese un primer numero entero");
+let ps1 = parseInt(s1);
+let s2 = prompt ("ingrese un segundo numero entero diferente del primero");
+var ps2 = parseInt(s2);
+alert(ps1+ps2)
 
 
 /*Resta*/
-var n1 = prompt ("Ingrese un primer numero entero");
-var n1 = parseInt(n1);
-var n2 = prompt ("ingrese un segundo numero entero diferente del primero");
-var n2 = parseInt(n2);
-alert(n1-n2)
+let r1 = prompt ("Ingrese un primer numero entero");
+let pr1 = parseInt(r1);
+let r2 = prompt ("ingrese un segundo numero entero diferente del primero");
+let pr2 = parseInt(r2);
+alert(pr1-pr2)
 
 
 /*División*/
@@ -24,7 +24,7 @@ var n1 = parseInt(n1);
 var n2 = prompt ("ingrese un segundo numero entero diferente del primero");
 var n2 = parseInt(n2);
 alert(n1/n2)
-git 
+
 /*Multiplicación*/
 
 var n1 = prompt ("Ingrese un primer numero entero");
@@ -40,3 +40,18 @@ var n1 = parseInt(n1);
 var n2 = prompt ("ingrese un segundo numero entero diferente del primero");
 var n2 = parseInt(n2);
 alert(n1%n2)
+
+/* temperatura en grados Kelvin*/
+
+var n1 = prompt ("Ingrese una temperatura en grados celcius para trasnformarla a Kelvin");
+var n1 = parseInt(n1);
+const kelvin = 273.15
+alert (n1+kelvin)
+
+/* temperatura en grados Fahrenheit*/
+
+var n1 = prompt ("Ingrese una temperatura en grados celcius para trasnformarla a Fahrenheit");
+var n1 = parseInt(n1);
+const Fahrenheit = 32
+alert ((n1* 9/5)+Fahrenheit)
+
---
+++ assets/js/scriptTemperatura.js
@@ -0,0 +1,19 @@
+
+/* 0 Grados Celsius corresponden a 273,15 Kelvin. La fórmula para convertirlos
+es 0 °C + 273.15 = 273.15 K
+● 0 Grados Celsius corresponden a 32 Fahrenheit. La fórmula para convertirlos
+es (0 °C × 9/5) + 32 = 32 °F */
+
+
+
+let n1 = prompt ("Ingrese una temperatura en grados celcius para trasnformarla a Kelvin");
+let n1 = parseInt(n1);
+const Kelvin = 273.15
+alert (n1+kelvin)
+
+
+let n1 = prompt ("Ingrese una temperatura en grados celcius para trasnformarla a Fahrenheit");
+let n1 = parseInt(n1);
+const Fahrenheit = 32
+alert ((n1* 9/5)+Fahrenheit)
+
